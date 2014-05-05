22b18324fc831a7354fc5eef59f08bb23f6021b0
kobapan <kobapan@gmail.com>
2014-05-05 14:59:07 +0900

    Create post-thumbnail.rb

---
+++ post-thumbnail.rb
@@ -0,0 +1,11 @@
+le Jekyll
+  module PostThumbnail
+    def thumbnail(input, h="100px")
+      if /<img(.*?)>|!\[\]\((.*?)\)/ =~ input
+        '<img' + $~[1] + 'height="' + h + '">'
+      end
+    end
+  end
+end
+
+Liquid::Template.register_filter(Jekyll::PostThumbnail)
