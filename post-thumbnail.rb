d7cb522d72671486c04b183236d6816102739e4f
kobapan <kobapan@gmail.com>
2014-05-05 15:13:47 +0900

    Update post-thumbnail.rb

--- post-thumbnail.rb
+++ post-thumbnail.rb
@@ -1,3 +1,13 @@
+# This is a Liquid filter plugin for jekyll , show the first image as an img tag .
+#
+# Usage
+#
+# {{ post.content | thumbnail }}
+# => <img src="http://exsample.com/1.jpg" height="100px">
+#
+# {{ post.content | thumbnail: '200px'}}
+# => <img src="http://exsample.com/1.jpg" height="200px">
+#
 module Jekyll
   module PostThumbnail
     def thumbnail(input, h="100px")
