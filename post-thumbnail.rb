465a79b259d5a5b954d5a75fc0181b2839603bc5
kobapan <kobapan@gmail.com>
2014-05-23 19:49:44 +0900

    Update post-thumbnail.rb

--- post-thumbnail.rb
+++ post-thumbnail.rb
@@ -3,16 +3,20 @@
 # Usage
 #
 # {{ post.content | thumbnail }}
-# => <img src="http://exsample.com/1.jpg" height="100px">
+# => <img src="http://exsample.com/1.jpg" height="100px" width="">
 #
 # {{ post.content | thumbnail: '200px'}}
-# => <img src="http://exsample.com/1.jpg" height="200px">
+# => <img src="http://exsample.com/1.jpg" height="200px" width="">
+#
+# {{ post.content | thumbnail: '' , '200px'}}
+# => <img src="http://exsample.com/1.jpg" height="" width="200px">
+#
 #
 module Jekyll
   module PostThumbnail
-    def thumbnail(input, h="100px")
+    def thumbnail(input, h="100px", w="" )
       if /<img(.*?)>|!\[\]\((.*?)\)/ =~ input
-        '<img' + $~[1] + ' height="' + h + '">'
+        '<img' + $~[1] + ' height="' + h + '" width="' + w + '">'
       end
     end
   end
